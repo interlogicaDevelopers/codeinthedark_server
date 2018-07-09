53983036562e5d2616e5e144deabede5c95ceb0f
Oscar Chinellato <oscar.chinellato@interlogica.it>
2018-07-09 14:51:44 +0200

    Added API support and dashboard gen

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-require('dotenv').config()
+require('dotenv').config();
 const _ = require('lodash');
 const express = require('express');
 const cors = require('cors');
 const aws = require('aws-sdk');
 const bodyParser = require('body-parser');
 const moment = require('moment');
+const auth = require('http-auth');
+
+const basic = auth.basic({
+    realm:"MyRealm",
+    file: __dirname + "/data/sec"
+});
 
 const app = express();
-app.use(cors())
+app.use(cors());
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const wrap = require('async-middleware').wrap;
 
-app.use(express.static('public'))
+
 app.use(bodyParser.json()); 
 
 
@@ -21,14 +27,14 @@ const {Player, Round, Vote} = require('./db');
 
 app.get('/', wrap(async (req, res) => {
     res.send('WELCOME')
-}))
+}));
 
 app.get('/round', wrap(async (req, res) => {
 
     const rounds = await Round.find();
     res.json(rounds)
     
-}))
+}));
 
 app.get('/round/:id', wrap(async (req, res) => {
 
@@ -36,8 +42,90 @@ app.get('/round/:id', wrap(async (req, res) => {
         id: req.params.id
     });
     res.json(round)
-    
-}))
+
+}));
+
+app.post('/round/start/:roundId', wrap(async (req, res) => {
+    await Round.update({_id: req.params.roundId}, {
+        voting: false,
+        running: true,
+        next: false,
+        showing_results: false
+    });
+
+    res.status(200);
+    res.end();
+
+}));
+
+app.post('/round/next/:roundId', wrap(async (req, res) => {
+    await Round.update({_id: req.params.roundId}, {
+        voting: false,
+        running: false,
+        next: true,
+        showing_results: false
+    });
+
+    res.status(200);
+    res.end();
+
+}));
+
+app.post('/round/stop/:roundId', wrap(async (req, res) => {
+    await Round.update({_id: req.params.roundId}, {
+        voting: false,
+        running: false,
+        next: false,
+        showing_results: false
+    });
+
+    res.status(200);
+    res.end();
+}));
+
+app.post('/round/archive/:roundId', wrap(async (req, res) => {
+    await Round.update({_id: req.params.roundId}, {
+        voting: false,
+        running: false,
+        next: false,
+        showing_results: false
+    });
+
+    res.status(200);
+    res.end();
+}));
+
+app.post('/round/startVote/:roundId', wrap(async (req, res) => {
+    await Round.update({_id: req.params.roundId}, {
+        voting: true,
+        running: false,
+        next: false,
+        showing_results: false
+    });
+
+    res.status(200);
+    res.end();
+}));
+
+app.post('/round/showResults/:roundId', wrap(async (req, res) => {
+
+    await Round.update({_id: req.params.roundId}, {
+        voting: false,
+        running: false,
+        next: false,
+        showing_results: true
+    });
+
+    res.status(200);
+    res.end();
+}));
+
+
+
+
+
+
+
 
 app.post('/vote/:roundId/:playerId', wrap(async (req, res) => {
 
@@ -45,18 +133,18 @@ app.post('/vote/:roundId/:playerId', wrap(async (req, res) => {
         vote_for: req.params.playerId,
         round: req.params.roundId,
         voter: req.body
-    })
+    });
     await vote.save();
     
-}))
+}));
 
 app.get('/vote/:roundId', wrap(async (req, res) => {
     const votes = await Vote.find({
         round: req.params.roundId
-    })
-    const groups = _.groupBy(votes, vote => vote.vote_for)
+    });
+    const groups = _.groupBy(votes, vote => vote.vote_for);
     res.json(groups)
-}))
+}));
 
 
 io.on('connection', (socket) => {
@@ -68,14 +156,14 @@ const checkRounds = async () => {
 
     const nextRound = await Round.findOne({
         next: true
-    })
+    });
 
     let missing;
 
     if (nextRound) {
-        console.log('FOUND NEXT ROUND')
+        console.log('FOUND NEXT ROUND');
 
-        missing = moment(nextRound.start).diff(moment())
+        missing = moment(nextRound.start).diff(moment());
 
         io.sockets.emit('message', {
             type: 'ROUND_COUNTDOWN',
@@ -88,12 +176,12 @@ const checkRounds = async () => {
 
     const runningRound = await Round.findOne({
         running: true
-    })
+    });
 
     if(runningRound) {
-        console.log('FOUND RUNNING ROUND')
+        console.log('FOUND RUNNING ROUND');
 
-        missing = moment(runningRound.end).diff(moment())
+        missing = moment(runningRound.end).diff(moment());
 
         io.sockets.emit('message', {
             type: 'ROUND_END_COUNTDOWN',
@@ -106,12 +194,12 @@ const checkRounds = async () => {
 
     const runningVote = await Round.findOne({
         voting: true
-    })
+    });
 
     if(runningVote) {
-        console.log('FOUND VOTE RUNNING')
+        console.log('FOUND VOTE RUNNING');
 
-        missing = moment(runningVote.end).diff(moment())
+        missing = moment(runningVote.end).diff(moment());
 
         io.sockets.emit('message', {
             type: 'VOTE_COUNTDOWN',
@@ -122,20 +210,55 @@ const checkRounds = async () => {
         })
     }
 
-}
+    const showingResultsRound = await Round.findOne({
+        showing_results: true
+    });
+
+    if(showingResultsRound) {
+        console.log('FOUND ROUND SHOWING RESULTS');
+
+        io.sockets.emit('message', {
+            type: 'SHOWING_RESULTS',
+            data: {
+                round: showingResultsRound
+            }
+        })
+    }
+
+};
+
 
 setInterval(async() => {
     await checkRounds()
-}, 1000)
-
+}, 1000);
 
 
 app.use((err, req, res, next) => {
-    console.log('ERROR')
+    console.log('ERROR');
     if (res.headersSent) {
         return next(err)
     }
     res.status(500).send(err.message)
-})
+});
+
+
+app.use(express.static('public'));
+
+// DASHBOARD
+
+app.use(auth.connect(basic));
+app.set('view engine', 'pug');
+
+app.get('/admin', wrap(async (req, res) => {
+
+    const rounds = await Round.find();
+
+    res.render('index', {
+        title: 'Admin DASHBOARD',
+        rounds
+    })
+
+}));
+
 
 server.listen(3000);
---
+++ public/assets/dashboard.js
@@ -0,0 +1,140 @@
+var DOMAIN = 'http://localhost:3000';
+
+function startRound(id) {
+
+    console.log('start Round', id);
+
+    fetch(DOMAIN + '/round/start/' + id, {
+        method: 'POST'
+    })
+        .then(response => {
+
+            if (response.status === 200) {
+                console.log('done');
+                window.location.reload(true);
+            } else {
+
+                alert('error!')
+            }
+
+
+
+        });
+
+
+}
+
+
+function nextRound(id) {
+
+    console.log('next Round', id);
+
+    fetch(DOMAIN + '/round/next/' + id, {
+        method: 'POST'
+    })
+        .then(response => {
+
+            if (response.status === 200) {
+                console.log('done');
+                window.location.reload(true);
+            } else {
+
+                alert('error!')
+            }
+
+
+
+        });
+
+
+}
+
+function stopRound(id) {
+    console.log('stop Round', id)
+
+    fetch(DOMAIN + '/round/stop/' + id, {
+        method: 'POST'
+    })
+        .then(response => {
+
+            if (response.status === 200) {
+                console.log('done');
+                window.location.reload(true);
+            } else {
+
+                alert('error!')
+            }
+
+
+
+        });
+
+}
+
+function archiveRound(id) {
+    console.log('archive round', id)
+
+    fetch(DOMAIN + '/round/archive/' + id, {
+        method: 'POST'
+    })
+        .then(response => {
+
+            if (response.status === 200) {
+                console.log('done');
+                window.location.reload(true);
+            } else {
+
+                alert('error!')
+            }
+
+
+
+        });
+
+}
+
+function startVote(id) {
+    console.log('start vote', id)
+
+    fetch(DOMAIN + '/round/startVote/' + id, {
+        method: 'POST'
+    })
+        .then(response => {
+
+            if (response.status === 200) {
+                console.log('done');
+                window.location.reload(true);
+            } else {
+
+                alert('error!')
+            }
+
+
+
+        });
+
+}
+
+
+function showResults(id) {
+    console.log('show results', id)
+
+
+    fetch(DOMAIN + '/round/showResults/' + id, {
+        method: 'POST'
+    })
+        .then(response => {
+
+            if (response.status === 200) {
+                console.log('done');
+                window.location.reload(true);
+            } else {
+
+                alert('error!')
+            }
+
+
+
+        });
+
+}
